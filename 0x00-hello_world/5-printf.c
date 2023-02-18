d446329ac9a78a116800dc7297941906e2cf516e
Flomutechnologies <flomutechnologies@gmail.com>
2023-02-18 03:03:05 -0800

    5th assignment

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio>
 /**
- * main -A program that prints usinf printf
+ * main - A program that prints usinf print a line using the printf function
+ * Return: 0 (Success)
  */
-int main(void);
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
