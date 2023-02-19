6f47dbee98f29a55cfda656740adfb75649cdee7
Flomutechnologies <flomutechnologies@gmail.com>
2023-02-19 09:35:58 -0800

    8th comiison

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Suuccess)
+ */
+
+int main(void)
+{
+	int d;
+	char low;
+
+	for (d = '0'; d <= 'f' ; low++)
+	putchar(low);
+	putchar('\n')
+
+	return (0);
+}
