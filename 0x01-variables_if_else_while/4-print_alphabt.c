4e0be6d05e26f26c1abc01ca83d423e2b5e289b3
Flomutechnologies <flomutechnologies@gmail.com>
2023-02-20 07:21:34 -0800

    4th

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,15 +11,14 @@ int main(void)
 {
 	char low, e, q;
 
-	e = 'e'
-	q = 'q'
+	e = 'e';
+	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++
 	{
-	if (low != e && low != g)
+	if (low != e && low != q)
 	putchar(low);
 	}
 	putchar('\n');
 	return (0);
 }
-			{
